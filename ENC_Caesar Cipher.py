8429c8c15d7e1c9222f31f8c5c1eb3b7154b6cf8
shiroyasha <83152234+Kyuusack@users.noreply.github.com>
2022-06-30 11:21:27 +0700

    Add files via upload

---
+++ ENC_Caesar Cipher.py
@@ -0,0 +1,29 @@
+from re import A
+
+def encrypt(text,s):
+    result = ""
+        # transverse uppercase characters in plain text
+    for i in range(len(text)):
+        char = text[i]
+            # Encrypt uppercase characters in plain text
+        if (char.isupper()):
+            result += chr((ord(char) + s - 65) % 26 + 65)
+        #elif (char.isdigit()):
+        #    result += chr((ord(char) + s - 48) % 10 + 48)
+        elif (char.islower()):
+            result += chr((ord(char) + s - 97) % 26 + 97)
+            # Space Transform to space
+        elif (char.isspace()):
+            result += chr((ord(char) + s + 32) % 1 + 32)
+            # Encrypt lowercase characters in plain text
+        else:
+            result += chr((ord(char) + s - 97) % 26 + 97)
+            
+    return result
+
+# Check the above function 
+text = "Akusuka kamu2000"
+s = 13
+print ('Plain text : ' + text)
+print ('Shift pattern : ' + str(s))
+print ('Cipher : ' + encrypt(text,s))
